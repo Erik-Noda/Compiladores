8538ae5fd3711a7bd2d2c5e379b11fb6df14a1c2
Erik-Noda <93678105+Erik-Noda@users.noreply.github.com>
2023-09-10 21:44:30 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,14 @@ package AnalisadorLexico;
 
 public class Main {
     public static void main(String[] args) {
-        LeitorArquivo ldat=new LeitorArquivo("C:\\Users\\a2367874\\Documents\\NetBeansProjects\\JavaApplication1\\src\\AnalisadorLexico\\programa1.gyh");
-        int c;
-        while((c=ldat.lerProximoCaractere())!= -1){
-            System.out.print((char)c);
+        //GyhLex lex = new GyhLex(args[0]);
+        Teste lex = new Teste(args[0]);
+        Token t;
+        while((t=lex.proximoToken())!=null) {
+            System.out.print(t.toString()+"\n");
         }
     }
+    
+    
 }
+

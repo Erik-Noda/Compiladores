9785babeb5732c71663eac6227eb195ed7bfebf9
Erik-Noda <93678105+Erik-Noda@users.noreply.github.com>
2023-09-12 18:17:35 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,7 @@ package AnalisadorLexico;
 
 public class Main {
     public static void main(String[] args) {
-        //GyhLex lex = new GyhLex(args[0]);
-        Teste lex = new Teste(args[0]);
+        GyhLex lex = new GyhLex(args[0]);
         Token t;
         while((t=lex.proximoToken())!=null) {
             System.out.print(t.toString()+"\n");

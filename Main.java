d1aa01ac0a7afb28b249e130626a588cf2a1bbc3
Erik-Noda <93678105+Erik-Noda@users.noreply.github.com>
2023-10-23 00:44:51 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 package AnalisadorLexico;
 
+import java.util.ArrayList;
+
 public class Main {
+    
     public static void main(String[] args) {
+        
         GyhLex lex = new GyhLex(args[0]);
         Token t;
+        ArrayList<Token>lista = new ArrayList<>();
+        
         while((t=lex.proximoToken())!=null) {
             System.out.print(t.toString()+"\n");
+            lista.add(t);
         }
+        Token tokenFinal = new Token("$",TipoToken.TokenFinal,-1);
+        lista.add(tokenFinal);
+        AnalisadorSintatico analisadorSintatico = new AnalisadorSintatico(lista);
+        analisadorSintatico.P();
     }
-    
     
 }
 
